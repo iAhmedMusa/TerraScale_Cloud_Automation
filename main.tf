84196d9646b9d2306f7ce14d9d049caf5a38ff9f
Ahmed Musa <ahmedmusa.swe@gmail.com>
2025-08-21 22:42:33 +0200

    add, Deploy S3 bucket for multiple stage, clean main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,16 @@
-module "s3_bucket" {
+# For single bucket deployment
+module "s3_bucket_single" {
   source             = "./modules/s3_bucket"
   bucket_name_prefix = var.bucket_name_prefix
   tags               = var.tags
+}
+
+# To deploy multiple bucket for multiple stage
+module "s3_bucket" {
+  source             = "./modules/s3_bucket"
+  for_each           = toset(["dev", "stage", "prod"])
+  bucket_name_prefix = "${var.bucket_name_prefix}-${each.key}"
+  tags = {
+    Environment = upper(each.key)
+  }
 }
